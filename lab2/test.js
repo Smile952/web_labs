c3f6dac74f50766ada11a69e7d96a68adaaff33c
Smile952 <deniska1234denis@gamil.com>
2024-11-06 11:16:26 +0700

    some config with calculate

--- lab2/test.js
+++ lab2/test.js
@@ -1,11 +1,21 @@
 var arg_first = ''
 var arg_second = ''
+
+var first_value = 0
+var second_value = 0
+
 var operation = ''
+
 var arg_switch = 0
+
 var expression = ''
+
 var isNext = 0
+
 var color_switch = 0
+
 var first_operation = '+';
+
 window.onload = function(){
     const display_expression = document.getElementById('disp1');
     const display_ans = document.getElementById('disp2')
@@ -14,95 +24,34 @@ window.onload = function(){
     var calc = ''
     
     buttons_digit.forEach(el=>{
-            el.onclick=function(){on_button_press(el)}
+            el.onclick=function(){updateDisplay(el, true)}
                                 }
                             )
 
     buttons_operation.forEach(el=>{
-            el.onclick=function(){on_button_press(el)}
+            el.onclick=function(){updateDisplay(el, false)}
                             }
                         )
 
-    function on_button_press(btn){
-        if(is_digit_button(btn) & btn.id !== 'cls'){
-            if(arg_switch === 0){
-                expression+=btn.id
-                arg_first+=btn.id
-            }
-            else{
-                expression+=btn.id
-                arg_second+=btn.id;
-            }
-            display_expression.innerHTML = expression
-        }
-        else if(is_operation_button(btn)){
-            if(arg_first === '' && (btn.id === '-' || btn.id === '+')){
-                expression+=btn.id
-                first_operation = first_operation === '-' ? '+' : '-';
-            }
-            else{
-                operation += btn.id;
-                expression+=btn.id
-            }
-            if(isNext === 0){
-                arg_switch = arg_switch === 0 ? 1 : 0
-            }
-            display_expression.innerHTML = expression
-        }
-        else if(btn.id === 'cls'){
-            clear_expression()
-        }
-        else if(btn.id === '='){
-            calc = calculate()
-            display_ans.innerHTML = expression
-            display_expression.innerHTML = calc;
-            expression = calc
-            arg_first = calc
-            arg_second = ''
-            operation = ''
-            isNext = 1
-        }
-        else{
-            changeBackground()
-        }
-    }
-
-    function is_digit_button(btn){
-        if(btn.id === '+' || btn.id === '-' || btn.id === '*' ||btn.id === '/' || btn.id ==='=' || btn.id === 'ct' || btn.id === 'cls'){
-            return false;
-        }
-        return true;
-    }
-
-    function is_operation_button(btn){
-        if(btn.id === '+' || btn.id === '-' || btn.id === '*' || btn.id === '/'){
-            return true;
-        }
-        return false;
-    }
-
     function calculate(){
-        arg1 = parseFloat(arg_first)
-        if(first_operation === '-'){
-            arg1*=-1;
-        }
-        arg2 = parseFloat(arg_second)
-        ans = 0
         switch(operation){
             case '+':
-                ans = arg1+arg2
-                break;
+                return String(first_value + second_value)
             case '-':
-                ans = arg1-arg2
-                break;
+                return String(first_value - second_value)
             case '*':
-                ans = arg1*arg2
-                break;
+                return String(first_value * second_value)
             case '/':
-                ans = arg1/arg2
-                break;
+                return String(first_value / second_value)
+            case '%':
+                return String(first_value * second_value / 100)
+            case 'sqrt':
+                return String(Math.pow(first_value, 1/2));
+            case 'pow':
+                return String(Math.pow(first_value, 2))
+            case 'fac':
+                return String(factorial(first_value))
         }
-        return String(ans)
     }
     function changeBackground() {
         if(color_switch === 1){
@@ -114,7 +63,7 @@ window.onload = function(){
             color_switch = 1;
         }
     }
-
+    //полная очистка введенных и рассчитаных данных
     function clear_expression(){
         first_operation = '+';
         isNext = 0
@@ -126,5 +75,104 @@ window.onload = function(){
         display_ans.innerHTML = ''
         display_expression.innerHTML = '0'
         calc = ''
+    }
+    function onDigitButtonPress(btn){
+        if(operation !== ''){
+            arg_second+=btn.id;
+            second_value = parseFloat(arg_second)
+            expression = arg_first + operation + arg_second
+        }
+        else{
+            arg_first+=btn.id;
+            first_value = parseFloat(arg_first);
+            expression = arg_first
+        }
+    }
+    function onOperatorButtonPress(btn){
+        switch(btn.id){
+            case "+":
+                operation = "+";
+                expression = arg_first + operation
+                break;
+            case "-":
+                operation = "-";
+                expression = arg_first + operation
+                break;
+            case "*":
+                operation = "*";
+                expression = arg_first + operation
+                break;
+            case "/":
+                operation = "/";
+                expression = arg_first + operation
+                break;
+            case "=":
+                calc = calculate();
+                display_ans.innerHTML = expression;
+                expression = calc;
+                arg_first = calc
+                first_value = parseFloat(calc);
+                arg_second = '';
+                second_value = 0;
+                operation = '';
+                break;
+            case "%":
+                operation = "%"
+                expression = arg_first + operation
+                break;
+            case "sign":
+                if(arg_second === ''){
+                   first_value*=-1
+                   arg_first = String(first_value)
+                   expression = first_value
+                }
+                break;
+            case ".":
+                break;
+            case "ct":
+                changeBackground();
+                break;
+            case "cls":
+                clear_expression();
+                break;
+            case "sqrt":
+                one_num_calc('sqrt')
+                break;
+            case 'pow':
+                one_num_calc('pow')
+                break;
+            case 'fac':
+                one_num_calc('fac')
+                break;
+        }
+       
+        return true;
+    }
+    function updateDisplay(btn, isDigit){
+        if(isDigit){
+            onDigitButtonPress(btn)
+        }
+        else {
+            onOperatorButtonPress(btn)
+        }
+        display_expression.innerHTML = expression;
+    }
+    function factorial(val){
+        let ans = 1;
+        for(i = 1; i <= val; i++){
+            ans*=i
+        }
+        return ans;
+    }
+    function one_num_calc(oper){
+        operation = oper;
+        display_ans.innerHTML = expression;
+        calc = calculate()
+        expression = calc;
+        arg_first = calc
+        first_value = parseFloat(calc);
+        arg_second = '';
+        second_value = 0;
+        operation = '';
     }
 }
