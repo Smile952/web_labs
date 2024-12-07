662f1d3532fa3c9d02fcb57666ed4e6bb01f0ec0
Smile952 <deniska1234denis@gamil.com>
2024-12-07 18:55:16 +0700

    rewriting history

--- lab3/components/main_page_data/Data.js
+++ lab3/components/main_page_data/Data.js
@@ -3,21 +3,21 @@ export class Data{
         return [
             {
                 id: 1,
-                src: "https://loremflickr.com/200/200?random=1",
+                src: "images\\finance.jpg",
                 alt: "Финансовое планирование",
                 title: "Финансовое планирование",
                 text: "Так финансы вы еще не планировали"
             },
             {
                 id: 2,
-                src: "https://loremflickr.com/200/200?random=2",
+                src: "images\\invest.jpg",
                 alt: "Инвестиционные консультации",
                 title: "Инвестиционные консультации",
                 text: "По таким инвестициям вас еще не консультировали"
             },
             {
                 id: 3,
-                src: "https://loremflickr.com/200/200?random=3",
+                src: "images\\asset.jpg",
                 alt: "Управление активами",
                 title: "Управление активами",
                 text: "Такими активами вы еще не управляли"
